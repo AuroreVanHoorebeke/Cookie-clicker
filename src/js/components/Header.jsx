20258b1c60170a1afdb9d824a4fb619556013306
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-02-28 23:16:11 +0100

    add styling

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(props){
         return(
         <>
-            <div className="header flex flex-row justify-between">
+            <div className="header text-2xl flex flex-row justify-between">
                 <div className="m-5">Cookie clicker</div>
                 <div className="m-5">{props.user}'s bakery</div>
             </div>
