05b01f5ff02c4c1c4cb10eaada5af16e6d4d9ea6
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-02-28 18:37:12 +0100

    add bg color

--- src/js/components/GameDisplay.jsx
+++ src/js/components/GameDisplay.jsx
@@ -6,7 +6,7 @@ import Game from './Game.jsx';
 export default function GameDisplay(props){
     return(
         <>
-            <div className="clickingZone">
+            <div className="clickingZone bg-yellow-900 p-5">
                 <Game />
             </div>
         </>
