530044950e2df016449599ca1335f1e48efc5a50
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-01 01:01:02 +0100

    add user prompt

--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import '../index.css';
 
 import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 import GameDisplay from './components/GameDisplay.jsx'
 
 const app = document.querySelector("#app");
-//let user = prompt("Hello there, what's your name?");
+let user = prompt("Hello there, what's your name?");
+alert(`Hello, ${user}! Have fun playing my version of cookie clicker!`)
 
 ReactDOM.render(
     <>
-        <Header user='Aurore'/>
+        <Header user={user}/>
 
         <GameDisplay />
     </>,
