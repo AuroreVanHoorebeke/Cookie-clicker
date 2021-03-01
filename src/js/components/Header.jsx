0c44ab3b1f3107cd5476d781cbbc7e881fd1cc88
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-01 01:02:38 +0100

    change header styling

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header(props){
         return(
         <>
-            <div className="header text-2xl flex flex-row justify-between">
+            <div className="header text-2xl flex flex-row justify-between rounded-t-md bg-yellow-50 mt-10">
                 <div className="m-5">Cookie clicker</div>
                 <div className="m-5">{props.user}'s bakery</div>
             </div>
